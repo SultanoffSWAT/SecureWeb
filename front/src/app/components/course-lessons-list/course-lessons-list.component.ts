44994fdb8226e93c97effef9704c02d4be337710
aknur <95418973+aknurkappar@users.noreply.github.com>
2024-02-29 00:44:34 +0600

    Front, Home page

---
+++ front/src/app/components/course-lessons-list/course-lessons-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-lessons-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-lessons-list.component.html',
+  styleUrl: './course-lessons-list.component.css'
+})
+export class CourseLessonsListComponent {
+
+}
