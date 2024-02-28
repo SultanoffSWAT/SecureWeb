7f4c3e7dbb8f5b5d1ecc275ab369798d8c0fc5f2
aknur <95418973+aknurkappar@users.noreply.github.com>
2024-02-29 01:07:32 +0600

    Front, Authentication pages

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {SignInComponent} from "./components/sign-in/sign-in.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
+import {HomeComponent} from "./components/home/home.component";
+import {CoursesComponent} from "./components/courses/courses.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "sign-in",component: SignInComponent},
+  {path: "sign-up",component: SignUpComponent},
+  {path: "home",component: HomeComponent},
+  {path: "courses",component: CoursesComponent},
+
+];
